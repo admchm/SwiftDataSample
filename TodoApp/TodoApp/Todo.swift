b27d4ff5efa6e0f49c447b5d48e3d56be7057ed0
Adam Chomicki <admchm@outlook.com>
2023-08-08 13:14:50 +0200

    Add Todo, Tag and RGBColor classes

--- TodoApp/TodoApp/Todo.swift
+++ TodoApp/TodoApp/Todo.swift
@@ -7,12 +7,77 @@
 
 import Foundation
 import SwiftData
+import SwiftUI
 
 @Model
 final class Todo {    // we must use classes here
+    // TODO: - check other attributes that we could use like:
+    // @Attribute(originalName: "creationDate_") - renaming
+    @Attribute(.unique)
     var creationDate: Date
+    var name: String
+    var isDone: Bool
+    var priority: Int
     
-    init(timestamp: Date) {
-        self.creationDate = timestamp
+    @Attribute(.externalStorage)
+    var image: Data?
+    
+    // creating relationship between Todo and Tag classes
+    var tags: [Tag]?
+    
+    init(name: String, isDone: Bool = false, priority: Int = 0) {
+        self.creationDate = Date()
+        self.name = name
+        self.isDone = isDone
+        self.priority = priority
+    }
+}
+
+@Model
+final class Tag {
+    var name: String
+    
+    // relationships
+    var todos: [Todo]?
+    var color: RGBColor
+    
+    init(name: String, color: RGBColor) {
+        self.name = name
+        self.color = color
+    }
+}
+
+// When the property is stored, Codable is used for generating data blob
+// and then store this data blob in the database
+struct RGBColor: Codable {
+    let red: Double
+    let green: Double
+    let blue: Double
+    let alpha: Double
+    
+    init(red: Double, green: Double, blue: Double, alpha: Double) {
+        self.red = red
+        self.green = green
+        self.blue = blue
+        self.alpha = alpha
+    }
+    
+    init(_ color: Color) {
+        let uiColor = UIColor(color)
+        var red: CGFloat = 0.0
+        var green: CGFloat = 0.0
+        var blue: CGFloat = 0.0
+        var alpha: CGFloat = 0.0
+        
+        uiColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
+        self.red = Double(red)
+        self.green = Double(green)
+        self.blue = Double(blue)
+        self.alpha = Double(alpha)
+    }
+    
+    func toSwiftColor() -> Color {
+        return Color(red: red, green: green, blue: blue, opacity: alpha)
     }
 }
